9a295ab4fb2e62f7a8c0b0e336a2e8452327811d
KamilaBalak <>
2021-04-19 15:55:31 +0000

    Aufgabe 2

---
+++ summieren.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+int a;
+int b;
+int c;
+
+std::ifstream fin("daten.txt");
+std::ofstream fout("datensumme.txt");
+
+for (int n = 0; n<234; ++n)
+{
+  fin >> a >> b;
+  c = a + b;
+  std::cout << a << "," << b << std::endl;
+  std::cout << "Summe:" << c << std::endl;
+
+  fout << c << std::endl;
+}
+
+fin.close();
+fout.close();
+
+}
